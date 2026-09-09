550e4aee49cd8a7c0e36f02e78bb8596e29d91b8
JULIO CEZAR GRANDE DE LIMA <julio.lima23@senaisp.edu.br>
2026-09-09 16:38:58 -0300

    Estrutura da classe caminhão completa

--- sistema-de-entrega/src/Caminhao.java
+++ sistema-de-entrega/src/Caminhao.java
@@ -1,2 +1,38 @@
 public class Caminhao extends Veiculo {
+    private double capacidadeMaxima;
+    private double pesoCargaAtual;
+
+    public Caminhao(String placa, String modelo, double custoPorKm, double capacidadeMaxima) {
+        super(placa, modelo, custoPorKm);
+        this.capacidadeMaxima = capacidadeMaxima;
+
+        this.pesoCargaAtual = 0;
+
+
+    }
+
+    public void carregar(double pesoCargaAtual){
+        if (pesoCargaAtual > this.capacidadeMaxima){
+            System.out.println("O peso ultrapassa o limite suportado pelo caminhão");
+        } else {
+            this.pesoCargaAtual = pesoCargaAtual;
+        }
+    }
+
+    public double getCapacidadeMaxima(){
+        return this.capacidadeMaxima;
+    }
+
+    public  double getPesoCargaAtual(){
+        return this.pesoCargaAtual;
+    }
+
+    @Override
+    public double calcularCustoViagem(double distancia) {
+        if (distancia <= 0){
+            System.out.println("A distancia não pode ser menor ou igual a 0");
+        }
+        double taxaAdicionalCarga = pesoCargaAtual * 0.0325;
+        return (distancia * getCustoPorKm()) + taxaAdicionalCarga;
+    }
 }
