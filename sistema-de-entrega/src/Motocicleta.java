060f6c0c0c6e44961768d4adbe51ab2261f45061
JULIO CEZAR GRANDE DE LIMA <julio.lima23@senaisp.edu.br>
2026-09-09 16:18:53 -0300

    Classes Motocicleta e Carro foram ajustadas para não receber um valor menor que 0

--- sistema-de-entrega/src/Motocicleta.java
+++ sistema-de-entrega/src/Motocicleta.java
@@ -5,6 +5,9 @@ public class Motocicleta extends Veiculo{
 
     @Override
     public double calcularCustoViagem(double distancia) {
+        if (getCustoPorKm() <= 0){
+            System.out.println("O custo por km não pode ser menor ou igual a 0");
+        }
         return distancia * getCustoPorKm();
     }
 }
